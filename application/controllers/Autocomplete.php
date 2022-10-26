d0aae37cecb4937b668d9e400206099c4b26a8ce
Munawar Ahmad <anwarahmad391@gmail.com>
2022-10-26 16:02:23 +0700

    Perubahan tanggl 26-10-2022

--- application/controllers/Autocomplete.php
+++ application/controllers/Autocomplete.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Autocomplete extends CI_Controller {
 
 	public function __construct(){
+		// Tambah komentar
 		parent::__construct();
 		$this->load->library('libraryku');
 		$this->load->helper('helperku');
